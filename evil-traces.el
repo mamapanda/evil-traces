2d308538900f4a7be8e2a2bafce3cc25aae1c342
panda <daniel.phan36@gmail.com>
2019-07-03 19:33:39 -0700

    add :global runner

--- evil-traces.el
+++ evil-traces.el
@@ -121,6 +121,23 @@ NAME, FLAG, RANGE, and FACE are the same as `evil-traces--simple-hl'."
   (with-current-buffer evil-ex-current-buffer
     (evil-traces--simple-hl name flag (or range (evil-ex-full-range)) face)))
 
+(defun evil-traces--window-ranges (buffer)
+  "Calculate the ranges covered by BUFFER's active windows."
+  (let ((ranges (cl-loop for window in (get-buffer-window-list buffer nil t)
+                         collect (cons (window-start window) (window-end window))))
+        combined-ranges)
+    (setq ranges (cl-sort ranges #'< :key #'car))
+    (while (>= (length ranges) 2)
+      (let ((range-1 (pop ranges))
+            (range-2 (pop ranges)))
+        (if (>= (cdr range-1) (car range-2))
+            (push (cons (car range-1) (max (cdr range-1) (cdr range-2))) ranges)
+          (push range-1 combined-ranges)
+          (push range-2 ranges))))
+    (when ranges
+      (push (pop ranges) combined-ranges)) ; only one range is left
+    (nreverse combined-ranges)))
+
 ;; ** Default
 (defface evil-traces-default-face '((t (:inherit region)))
   "The default face for evil-traces overlays.")
@@ -158,13 +175,104 @@ FLAG is as described in `evil-ex-define-argument-type'."
 (evil-ex-define-argument-type evil-traces-default-buffer
   :runner evil-traces-hl-range-or-buffer)
 
+;; ** Global
+(defface evil-traces-global-range-face '((t :inherit evil-traces-default-face))
+  "The face for :global's range.")
+
+(defface evil-traces-global-match-face '((t :inherit evil-traces-default-face))
+  "The face for matched :global terms.")
+
+(defun evil-traces--global-match-name (number)
+  "Return a match identifier based on NUMBER."
+  (intern (format "evil-traces-global-match-%s" number)))
+
+(defun evil-traces--global-matches (pattern &optional beg end)
+  "Find :global matches for PATTERN between BEG and END.
+If there are multiple matches in a line, only the first is considered."
+  (let ((case-fold-search (eq (evil-ex-regex-case pattern evil-ex-search-case) 'insensitive))
+        (beg (or beg (point-min)))
+        (end (or end (point-max))))
+    (save-excursion
+      (save-match-data
+        (goto-char beg)
+        (cl-loop while (< (point) end)
+                 when (re-search-forward pattern (line-end-position) t)
+                 collect (cons (match-beginning 0) (match-end 0))
+                 do (forward-line))))))
+
+(defun evil-traces--delete-global-match-overlays (start end)
+  "Delete :global match overlays for matches numbered from START to END, exclusive."
+  (cl-loop for i from start below end
+           for ov-name = (evil-traces--global-match-name i)
+           do (evil-traces--delete-overlay ov-name)))
+
+(defvar evil-traces--last-global-params nil
+  "The last parameters passed to :global.")
+
+(defvar evil-traces--last-global-match-count 0
+  "The number of matches found by the last :global update.")
+
+(defun evil-traces--update-global (pattern range)
+  "Highlight RANGE and :global matches for PATTERN in RANGE."
+  (with-current-buffer evil-ex-current-buffer
+    (let* ((beg (evil-range-beginning range))
+           (end (evil-range-end range))
+           (params (list range pattern))
+           (match-count 0))
+      ;; don't have to update if the call is the exact same
+      (unless (equal evil-traces--last-global-params params)
+        (evil-traces--make-or-move-overlay
+         'evil-traces-global-range beg end 'face 'evil-traces-global-range-face)
+        (when pattern
+          (condition-case nil
+              (dolist (win-range (evil-traces--window-ranges (current-buffer)))
+                (dolist (match-bounds (evil-traces--global-matches
+                                       pattern (max beg (car win-range)) (min end (cdr win-range))))
+                  (evil-traces--make-or-move-overlay (evil-traces--global-match-name match-count)
+                                                     (car match-bounds)
+                                                     (cdr match-bounds)
+                                                     'face
+                                                     'evil-traces-global-match-face)
+                  (cl-incf match-count)))
+            (invalid-regexp nil))) ; trailing backslash
+        (evil-traces--delete-global-match-overlays match-count evil-traces--last-global-match-count)
+        (setq evil-traces--last-global-params params
+              evil-traces--last-global-match-count match-count)))))
+
+(defun evil-traces-hl-global (flag &optional arg)
+  "Highlight :global's range and matches.
+FLAG is one of 'start, 'update, or 'stop and signals what to do.
+ARG is the ex argument to :global."
+  (cl-case flag
+    (start
+     (setq evil-traces--last-global-params nil
+           evil-traces--last-global-match-count 0))
+    (update
+     (let ((range (or evil-ex-range
+                      (with-current-buffer evil-ex-current-buffer
+                        (evil-ex-full-range))))
+           ;; if ARG is nil or "/", leave pattern as nil instead of the last pattern
+           (pattern (and (> (length arg) 1)
+                         (condition-case nil
+                             (cl-first (evil-ex-parse-global arg))
+                           (user-error nil))))) ; no previous pattern
+       (evil-traces--run-timer #'evil-traces--update-global pattern range)))
+    (stop
+     (evil-traces--cancel-timer)
+     (evil-traces--delete-overlay 'evil-traces-global-range)
+     (evil-traces--delete-global-match-overlays 0 evil-traces--last-global-match-count))))
+
+(evil-ex-define-argument-type evil-traces-global
+  :runner evil-traces-hl-global)
+
 ;; * Minor Mode
 ;; TODO: implement the types
 (defcustom evil-traces-argument-type-alist
   ;; TODO: sort these in alphabetical order?
+  ;; TODO: change, copy, sort, join, yank, delete, and normal can have special runners
   '(
     (evil-ex-global . evil-traces-global)
-    (evil-ex-global-inverted . evil-traces-global-inverted)
+    (evil-ex-global-inverted . evil-traces-global)
     (evil-ex-join . evil-traces-default-line)
     (evil-ex-sort . evil-traces-default-buffer)
     (evil-change . evil-traces-default-line)
