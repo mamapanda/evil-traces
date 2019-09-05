890d9e518e0be901c947840ee70628c19185ee10
panda <daniel.phan36@gmail.com>
2019-09-05 11:47:41 -0700

    Make commentary format nicer

--- evil-traces.el
+++ evil-traces.el
@@ -27,10 +27,9 @@
 
 ;;; Commentary:
 ;;
-;; evil-traces is a port of traces.vim
-;; (https://github.com/markonm/traces.vim).  It adds visual hints to
-;; certain `evil-ex' commands.  To enable the hints, turn on
-;; `evil-traces-mode'.
+;; evil-traces is a port of traces.vim (https://github.com/markonm/traces.vim).
+;; It adds visual hints to certain `evil-ex' commands.  To enable the hints,
+;; turn on `evil-traces-mode'.
 
 ;;; Code:
 
