82e8a7b4213aed140f6eb5f2cc33a09bb5587166
Daniel Phan <daniel.phan36@gmail.com>
2023-08-20 15:42:09 -0700

    fix: allow evil-ex-current-buffer in test

--- test/make-compile.el
+++ test/make-compile.el
@@ -2,6 +2,11 @@
 (setq byte-compile-error-on-warn t)
 (setq byte-compile--use-old-handlers nil)
 
+;; The new variable was added fairly recently as of writing, so use the original
+;; one for better compatibility for now.
+(with-eval-after-load "evil"
+  (push 'evil-ex-current-buffer byte-compile-not-obsolete-vars))
+
 ;; compile *.el files
 (dolist (file (file-expand-wildcards "evil-traces*.el"))
   (unless (byte-compile-file file)
